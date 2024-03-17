08356f4acc1667d0890df6293f559aa8e052a251
sanketmali011 <sanket@lucy>
2024-03-17 12:48:02 +0530

    same

---
+++ pro1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class pro1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        System.out.println(rows + " " + cols);
+        int[][] number = new int[rows][cols];
+        System.out.println(number);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                number[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                System.out.println(number[i][j]+"");
+            }
+        }
+        System.out.println();
+    }
+}
